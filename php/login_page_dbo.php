9e92294586a076378bdae721c82ca18977832da3
Carlos Bazilio <carlosbazilio@gmail.com>
2016-11-18 17:04:44 -0200

    Inserção de nova consulta e exibição dos resultados

--- php/login_page_dbo.php
+++ php/login_page_dbo.php
@@ -22,15 +22,33 @@
 
 	    $sth = $conn->prepare("SELECT * FROM public.usuario WHERE login = '$user'");
 	    $sth->execute();
+	    // http://php.net/manual/pt_BR/pdostatement.fetchall.php
 	    $results = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-	    if ($results[0]['password'] == $password)
+	    if ($results[0]['password'] == $password) {
 	    	echo "Login successful!<br/>";
+	    	 
+	    	$sth = $conn->prepare("SELECT * FROM public.company");
+	    	$sth->execute();
+	    	$results = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+	    	//var_dump($results);
+
+	    	echo '<table border=1>';
+	    	echo '<tr><th>Nome</th><th>Empresa</th></tr>';
+		    	foreach ($results as $value) {
+		    		echo '<tr>';
+		    		echo '<td>' . $value['name'] . '</td><td>' . $value['address'] . '</td>';
+		    		echo '</tr>';
+		    	}
+		    echo '</table>';
+	    }
 	    else {
 	    	//var_dump($results);
 	    	echo "Login fail!<br/>";
 	    }
 
+		$sth = null;
 	    $conn = null;
 	}
 	catch(PDOException $e)
