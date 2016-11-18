64564fbeef88ed20eb6c9c2d6075eaf517da6239
Carlos Bazilio <carlosbazilio@gmail.com>
2016-11-18 17:25:45 -0200

    Inserção de comentários

--- js/ajax.js
+++ js/ajax.js
@@ -1,18 +1,21 @@
 function loadXMLDocAsync(url, campo)
 {
     var xmlhttp;
+    // http://www.w3schools.com/xml/ajax_xmlhttprequest_create.asp
     if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
+    {   // code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp=new XMLHttpRequest();
     }
     else
-    {// code for IE6, IE5
+    {   // code for IE6, IE5
         xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     }
+    // http://www.w3schools.com/xml/ajax_xmlhttprequest_response.asp
     xmlhttp.onreadystatechange=function()
     {
         if (xmlhttp.readyState==4 && xmlhttp.status==200)
         {
+            // http://www.w3schools.com/jsref/dom_obj_document.asp
             document.getElementById(campo).innerHTML=xmlhttp.responseText;
         }
     }
