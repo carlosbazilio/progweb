379eafd1d605a563a70b4011bf815411081d83a7
bazilio <carlosbazilio@gmail.com>
2021-02-09 23:36:15 -0300

    Versão inicial

---
+++ php-basico/cep.php
@@ -0,0 +1,26 @@
+<?php
+	function consultaCEP($valor) {
+		// create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, "https://viacep.com.br/ws/" . $valor . "/xml/");
+
+        //return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // $output contains the output string
+        $output = curl_exec($ch);
+
+        // close curl resource to free up system resources
+        curl_close($ch);
+
+        echo $output;
+	}
+
+	$valor = $_POST["valor"];
+	if (!is_null($valor))
+		echo consultaCEP($valor);
+	else
+		echo "CEP não informado!";
+?>
